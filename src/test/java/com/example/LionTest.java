a4de84f116acd5ebc16bccb27072926f2c927691
Irina Gladkova <igla210770@yandex.ru>
2023-06-28 19:04:51 +0300

    seventh commit

--- src/test/java/com/example/LionTest.java
+++ src/test/java/com/example/LionTest.java
@@ -1,10 +1,9 @@
 package com.example;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.List;
@@ -12,29 +11,52 @@ import static org.junit.Assert.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
- @Spy
-  Feline feline;
-  private Lion lionMale;
-  private Lion lionFemale;
+    @Mock
+    Feline feline;
 
     @Before
-   public void setLionMale() throws Exception {lionMale = new Lion("Самец", feline);}
+    public void setLionMale() throws Exception {
+        lionMale = new Lion("Самец", feline);
+    }
+
     @Before
-    public void setLionFemale() throws Exception {lionFemale = new Lion("Самка", feline);}
+    public void setLionFemale() throws Exception {
+        lionFemale = new Lion("Самка", feline);
+    }
+
+    private Lion lionMale;
+    private Lion lionFemale;
+
     @Test
     public void leonMaleEatTest() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         assertEquals("Это еда не для льва", List.of("Животные", "Птицы", "Рыба"), lionMale.getFood());
     }
+
     @Test
     public void leonFemaleEatTest() throws Exception {
+        Mockito.when(feline.getFood("Хищник")).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         assertEquals("Это еда не для львицы", List.of("Животные", "Птицы", "Рыба"), lionFemale.getFood());
     }
     @Test
+    public void getFootTest() throws Exception {
+        lionMale.getFood();
+        Mockito.verify(feline, Mockito.times(1)).getFood("Хищник");
+    }
+    @Test
     public void lionMaleKittensTest() {
-              assertEquals(1, lionMale.getKittens());
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1, lionMale.getKittens());
     }
     @Test
     public void lionFemaleKittensTest() {
+        Mockito.when(feline.getKittens()).thenReturn(1);
         assertEquals(1, lionFemale.getKittens());
     }
- }
+    @Test
+    public void getKittensTest() {
+        lionFemale.getKittens();
+        Mockito.verify(feline, Mockito.times(1)).getKittens();
+    }
+
+}
